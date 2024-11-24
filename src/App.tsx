082039e69f52400c0a7e0464b55ec68643647357
SDKishor <kishorsutradhar32@gmail.com>
2024-11-24 18:13:56 +0600

    project init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ModeToggle } from './components/themeModeToggle';
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <>
+      <p className="text-2xl font-bold">Hello</p>
+      <Button>Button</Button>
+      <ModeToggle />
+    </>
+  );
+}
+
+export default App;
